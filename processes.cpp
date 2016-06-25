ad6a409838030e5394d9a8968a1341902fff9a34
oscar-gt <oscar.garcia_telles@hotmail.com>
2016-06-25 00:01:11 -0700

    a few more lines

--- processes.cpp
+++ processes.cpp
@@ -5,11 +5,37 @@
  *      Author: Oscar Garcia-Telles
  */
 
+#include <iostream>
+using namespace std;
+
+
 //
 int main(int argc, char* argv[])
 {
+	bool verbose = true;
 
-	// Checking for correct number of arguments
+	// Checking for correct number of arguments.
+	// Should be 2: processes <command>
+	if(argc != 2)
+	{
+		cerr << "Two Argument Usage:  " << argv[0] << "  [command]" << endl;
+		return -1;
+	}
+
+	// Correct argument input at this point, continuing...
+	string command = argv[1];
+	if(verbose)
+	{
+		cout << "argv[1] == " << command << endl;
+	}
+
+	// ***********	Simulating the following command line input:	****************
+	// 						ps -A | grep [command] | wc -l
+	// where:
+	// child process executes 				wc -l
+	//
+	// grand child process executes 		grep [command]
+	// great grand child process executes 	ps -A
 }
 
 
