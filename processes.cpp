4d2519c3d5cf6d2cc56d31c943cfc431e0ab5c1c
oscar-gt <oscar.garcia_telles@hotmail.com>
2016-06-24 21:53:43 -0700

    first commit

---
+++ processes.cpp
@@ -0,0 +1,17 @@
+/*
+ * processes.cpp
+ *
+ *  Created on: Jun 24, 2016
+ *      Author: Oscar Garcia-Telles
+ */
+
+//
+int main(int argc, char* argv[])
+{
+
+	// Checking for correct number of arguments
+}
+
+
+
+
